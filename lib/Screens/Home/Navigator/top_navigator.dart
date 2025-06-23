7c93501d74c0ab344b8a2127e4abb4923dfda6f8
Saad Ahmed <saadahmed0147@gmail.com>
2025-06-23 17:14:29 +0500

    Added top navigator

---
+++ lib/Screens/Home/Navigator/top_navigator.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TopNavigator extends StatefulWidget {
+  const TopNavigator({super.key});
+
+  @override
+  State<TopNavigator> createState() => _TopNavigatorState();
+}
+
+class _TopNavigatorState extends State<TopNavigator> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
